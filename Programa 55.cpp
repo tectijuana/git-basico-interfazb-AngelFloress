0e5e7735579cb52f17fdb0862b327d020b76f743
Angel Flores <89488077+AngelFloress@users.noreply.github.com>
2022-03-24 22:40:49 -0700

    Create Programa 55.cpp

---
+++ Programa 55.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int edades[20];
+	for (int i=0;i<20;i++)
+	{
+		if(i<6)
+		{
+			edades[i] = 18;
+		}
+		if (i >= 6 && i < 17)
+		{
+			edades[i] = 19;
+		}
+		if(i>16)
+		{
+			edades[i] = 20;
+		}
+	}
+	cout << "La edad del alumno random es de " << edades[rand() % 20] << " años" << endl;
+}
